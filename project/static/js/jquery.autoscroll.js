0beb0bf8e1f061894f48523e5d134c6b923f4c32
zuber <marek@stepniowski.com>
2009-08-18 12:50:57 +0200

    Dodanie możliwości włączania i wyłączania automatycznego przewijania.

--- project/static/js/jquery.autoscroll.js
+++ project/static/js/jquery.autoscroll.js
@@ -1,6 +1,7 @@
 (function($) {
     $.fn.autoscroll = function(synchronizeWith, options) {
-        var self = $(this);
+        var $this = $(this);
+        var self = $this;
         var selfContainer = self.parent();
         var synchronizeWith = $(synchronizeWith);
         var synchronizeWithContainer = synchronizeWith.parent();
@@ -17,15 +18,30 @@
             synchronizeWithContainer = $('body', eventContainer);
             synchronizeWith = synchronizeWithContainer;
         }
-
-        synchronizeWithContainer.data('autoscroll.lastCheckedScrollTop', synchronizeWithContainer.scrollTop());
+        
+        $this.data('autoscroll:enabled', true);
+        synchronizeWithContainer.data('autoscroll:lastCheckedScrollTop', synchronizeWithContainer.scrollTop());
         
         eventContainer.scroll(function() {
-            var distanceScrolled = synchronizeWithContainer.scrollTop() - synchronizeWithContainer.data('autoscroll:lastCheckedScrollTop');
-            var percentScrolled = distanceScrolled / synchronizeWith.height();
-            selfContainer.scrollTop(selfContainer.scrollTop() + percentScrolled * self.height());
+            if ($this.data('autoscroll:enabled')) {
+                var distanceScrolled = synchronizeWithContainer.scrollTop() - synchronizeWithContainer.data('autoscroll:lastCheckedScrollTop');
+                var percentScrolled = distanceScrolled / synchronizeWith.height();
+                selfContainer.scrollTop(selfContainer.scrollTop() + percentScrolled * self.height());
+            }
             synchronizeWithContainer.data('autoscroll:lastCheckedScrollTop', synchronizeWithContainer.scrollTop());
         });
-    };
+    },
+    
+    $.fn.enableAutoscroll = function() {
+        $(this).data('autoscroll:enabled', true);
+    },
+        
+    $.fn.disableAutoscroll = function() {
+        $(this).data('autoscroll:enabled', false);
+    },
+    
+    $.fn.toggleAutoscroll = function() {
+        $(this).data('autoscroll:enabled', !$(this).data('autoscroll:enabled'));
+    }
 })(jQuery);
 
