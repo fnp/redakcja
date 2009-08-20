33d3159145359df7638b3ac0268d4a400856a932
zuber <marek@stepniowski.com>
2009-08-20 12:15:19 +0200

    Sortowanie listy plików i folderów w get_image_folders i get_images_from_folder. Refs #56.

--- apps/explorer/models.py
+++ apps/explorer/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 
 def get_image_folders():
-    return [fn for fn in os.listdir(os.path.join(settings.MEDIA_ROOT, settings.IMAGE_DIR)) if not fn.startswith('.')]
+    return sorted(fn for fn in os.listdir(os.path.join(settings.MEDIA_ROOT, settings.IMAGE_DIR)) if not fn.startswith('.'))
 
 
 def get_images_from_folder(folder):
-    return [settings.MEDIA_URL + settings.IMAGE_DIR + '/' + folder + '/' + fn for fn 
+    return sorted(settings.MEDIA_URL + settings.IMAGE_DIR + '/' + folder + '/' + fn for fn 
             in os.listdir(os.path.join(settings.MEDIA_ROOT, settings.IMAGE_DIR, folder))
-            if not fn.startswith('.')]
+            if not fn.startswith('.'))
 
