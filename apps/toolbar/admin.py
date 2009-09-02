9661cd90418ab652e181f6e85c2b3532b8f0c8f5
Łukasz Rekucki <lrekucki@gmail.com>
2009-09-02 11:55:21 +0200

    Reverted toolbar/admin.py to previous state.

--- apps/toolbar/admin.py
+++ apps/toolbar/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from explorer import models
+from toolbar import models
 
-admin.site.register(models.Book)
-admin.site.register(models.PullRequest)
+
+class ButtonGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'position',)
+    search_fields = ('name', 'slug',)
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ('position',)
+
+admin.site.register(models.ButtonGroup, ButtonGroupAdmin)
+
+
+class ButtonAdmin(admin.ModelAdmin):
+    list_display = ('label', 'slug', 'tag', 'key', 'position',)
+    list_filter = ('group',)
+    search_fields = ('label', 'slug', 'tag', 'key',)
+    prepopulated_fields = {'slug': ('label',)}
+    filter_horizontal = ('group',)
+    list_editable = ('position',)
+
+admin.site.register(models.Button, ButtonAdmin)
 
