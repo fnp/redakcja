1cbb8af568a8c7016e4d88e3f77250b5b4b824d6
zuber <marek@stepniowski.com>
2009-09-10 15:23:47 +0200

    Usunięcie modelu Book z admina.

--- apps/explorer/admin.py
+++ apps/explorer/admin.py
@@ -4,5 +4,4 @@ from django.utils.translation import ugettext_lazy as _
 import explorer.models
 
 admin.site.register(explorer.models.EditorSettings)
-admin.site.register(explorer.models.Book)
 admin.site.register(explorer.models.EditorPanel)
