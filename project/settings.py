935076367b73bf9506751fe215cacc09c3d6e417
zuber <marek@stepniowski.com>
2009-09-29 23:34:33 +0200

    Wstępne wykorzystanie modułu logging.

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,19 @@ EDITOR_DEFAULT_SETTINGS = {
     ],
 }
 
+# Python logging settings
+import logging
+
+log = logging.getLogger('platforma')
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+
+# Import localsettings file, which may override settings defined here
 try:
     from localsettings import *
 except ImportError:
