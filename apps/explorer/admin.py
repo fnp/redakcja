54d7b5abd1eddc6f22f8d3df75334a54a1420485
zuber <marek@stepniowski.com>
2009-11-06 00:14:15 +0100

    Łatwiejsze wybieranie galerii dla dokumentów w interfejsie administracyjnym.

--- apps/explorer/admin.py
+++ apps/explorer/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
+from django import forms
+import os
+
+from django.conf import settings
 
 import explorer.models
 
 admin.site.register(explorer.models.EditorSettings)
 admin.site.register(explorer.models.EditorPanel)
-admin.site.register(explorer.models.GalleryForDocument)
+
+
+class GalleryAdminForm(forms.ModelForm):
+    subpath = forms.ChoiceField(choices=())
+    
+    def __init__(self, *args, **kwargs):
+        super(GalleryAdminForm, self).__init__(*args, **kwargs)
+        self.fields['subpath'].choices = [(x, x) for x in os.listdir(settings.MEDIA_ROOT + settings.IMAGE_DIR)]
+        
+    class Meta:
+        mode = explorer.models.GalleryForDocument
+        fields = ('document', 'subpath',)
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    form = GalleryAdminForm
+    list_display = ('document', 'subpath',)
+    search_fields = ('document', 'subpath',)
+
+admin.site.register(explorer.models.GalleryForDocument, GalleryAdmin)
