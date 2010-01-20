22aac0da99594406b261f14c135812c855c196ef
zuber <marek@stepniowski.com>
2010-01-20 17:41:48 +0100

    Usunięcie aplikacji api.

--- apps/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from api import models
-
-class PullRequestAdmin(admin.ModelAdmin):
-    list_display = ('comitter', 'timestamp', 'comment', 'document', 'source_revision')
-
-admin.site.register(models.PullRequest, PullRequestAdmin)
-admin.site.register(models.PartCache)
