5fa547431332f4d6e416ad8904d52c953a1eb309
zuber <marek@stepniowski.com>
2010-02-09 13:56:50 +0100

    Podlinkowanie logo do strony głównej.

--- platforma/urls.py
+++ platforma/urls.py
@@ -8,43 +8,9 @@ admin.autodiscover()
 PATH_SEC = r"(?P<path>[^/]+)"
 PATH_END = PATH_SEC + "/$"
 
-urlpatterns = patterns('',
-    # Explorer:
-    # url(r'^$', 'explorer.views.file_list', name='file_list'),        
-    # url(r'^file/upload', 'explorer.views.file_upload', name='file_upload'),    
 
-    # url(r'^management/pull-requests$', 'explorer.views.pull_requests'),
-  
-    # Editor panels
-    # url(r'^editor/'+PATH_END, 'explorer.views.display_editor', name='editor_view'),
-    # url(r'^editor/$', 'explorer.views.file_list', name='editor_base'),
 
-    # url(r'^file/(?P<docid>[^/]+)/print$', 'explorer.views.print_html', name="file_print"),
-
-    url(r'^$', 'wiki.views.document_list'),
-    
-    # Admin panel
-    (r'^admin/filebrowser/', include('filebrowser.urls')),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/(.*)', admin.site.root),
-
-    # Theme database
-    # url(r'themes/', include('bookthemes.urls')),
-
-    # Our über-restful api
-    # url(r'^api/', include('api.urls')),
-    
-    url(r'^gallery/(?P<directory>[^/]+)$', 'wiki.views.document_gallery'),
-    
-    # Static files (should be served by Apache)
-    url(r'^%s(?P<path>.+)$' % settings.MEDIA_URL[1:], 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'^%s(?P<path>.+)$' % settings.STATIC_URL[1:], 'django.views.static.serve',
-        {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
-        
-    url(r'^(?P<name>(.*))$', 'wiki.views.document_detail'),
-    
-)
+urlpatterns = patterns('')
 
 if 'cas_consumer' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
@@ -58,3 +24,23 @@ else:
         url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'redirect_field_name': 'next'}, name='login'),
         url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),    
     )
+
+urlpatterns += patterns('',
+    url(r'^$', 'wiki.views.document_list'),
+    url(r'^gallery/(?P<directory>[^/]+)$', 'wiki.views.document_gallery'),
+        
+    # Admin panel
+    (r'^admin/filebrowser/', include('filebrowser.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/(.*)', admin.site.root),
+    
+    # Static files (should be served by Apache)
+    url(r'^%s(?P<path>.+)$' % settings.MEDIA_URL[1:], 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'^%s(?P<path>.+)$' % settings.STATIC_URL[1:], 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+        
+    url(r'^(?P<name>(.*))$', 'wiki.views.document_detail'),
+    
+)
+
