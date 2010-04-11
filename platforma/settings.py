69ce61e6ef27d1911538523b57371e19926641cb
Łukasz Rekucki <lrekucki@gmail.com>
2010-04-11 21:05:41 +0200

    Bumped up python version in the template.

--- platforma/settings.py
+++ platforma/settings.py
@@ -27,7 +27,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Warsaw Poland'
+TIME_ZONE = 'Europe/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -94,7 +94,7 @@ AUTHENTICATION_BACKENDS = (
     'django_cas.backends.CASBackend',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'platforma.urls'
 
 TEMPLATE_DIRS = (
     PROJECT_ROOT + '/templates',
