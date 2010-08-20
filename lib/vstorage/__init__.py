b13038d172d0ac2bb1582452e55b43e727805031
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2010-08-20 12:10:24 +0200

    require and use Mercurial 1.6 API

--- lib/vstorage/__init__.py
+++ lib/vstorage/__init__.py
@@ -20,7 +20,6 @@ os.environ['HGMERGE'] = "internal:merge"
 import mercurial.hg
 import mercurial.revlog
 import mercurial.util
-from mercurial.context import workingctx
 
 from vstorage.hgui import SilentUI
 
@@ -220,7 +219,7 @@ class VersionedStorage(object):
             filectx_tip = changectx[repo_file]
             current_page_rev = filectx_tip.filerev()
         except mercurial.revlog.LookupError:
-            workingctx(self.repo).add([repo_file])
+            self.repo[None].add([repo_file])
             current_page_rev = -1
 
         if parent is not None and current_page_rev != parent:
@@ -272,7 +271,7 @@ class VersionedStorage(object):
             os.unlink(file_path)
         except OSError:
             pass
-        workingctx(self.repo).remove([repo_file])
+        self.repo[None].remove([repo_file])
         self._commit([repo_file], text, user)
 
     def page_text(self, title, revision=None):
