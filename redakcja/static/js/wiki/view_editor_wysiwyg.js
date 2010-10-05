298031e8a452a9012bc43098a32f60f5b5a2b152
root <root@debian.localdomain>
2010-10-05 08:05:34 -0500

    Ticket 818: better 'save' button styling.

--- redakcja/static/js/wiki/view_editor_wysiwyg.js
+++ redakcja/static/js/wiki/view_editor_wysiwyg.js
@@ -392,7 +392,7 @@
                     
                     var msg = $("<div class='saveNotify'><p>Twoje zmiany zostały naniesione na tekst źródłowy. Pamiętaj, że aby zmiany zostały utrwalone <span>należy je zapisać</span>!</p><p class='notifyTip'>Ta wiadomość zostanie automatycznie zamknięta za 6 sekund.</p></div>");
                     $("#base").prepend(msg);
-                    $("#save-button").css({border: '5px solid red'});
+                    $("#save-button").css({border: '2px solid #801000', backgroundColor: '#E1C1C1'});
                     $('#base .saveNotify').fadeOut(7000, function(){
                         $(this).remove(); 
                         $("#save-button").css({border: '1px solid black'});
