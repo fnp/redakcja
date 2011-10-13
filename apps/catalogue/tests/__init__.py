0d2710238c43e233ec76d8afa609704777807f42
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2011-10-13 11:49:15 +0200

    tests fix

--- apps/catalogue/tests/__init__.py
+++ apps/catalogue/tests/__init__.py
@@ -1,42 +1,51 @@
+from os.path import abspath, dirname, join
 from nose.tools import *
 from mock import patch
 from django.test import TestCase
 from django.contrib.auth.models import User
 from catalogue.models import Book, BookPublishRecord
 
+
+def get_fixture(path):
+    f_path = join(dirname(abspath(__file__)), 'files', path)
+    with open(f_path) as f:
+        return unicode(f.read(), 'utf-8')
+
+
 class PublishTests(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username='tester')
-        self.book = Book.create(self.user, 'publish me')
+        self.text1 = get_fixture('chunk1.xml')
+        self.book = Book.create(self.user, self.text1, slug='test-book')
 
     @patch('apiclient.api_call')
     def test_unpublishable(self, api_call):
-        with self.assertRaises(Book.NoTextError):
+        with self.assertRaises(AssertionError):
             self.book.publish(self.user)
 
     @patch('apiclient.api_call')
     def test_publish(self, api_call):
         self.book[0].head.set_publishable(True)
         self.book.publish(self.user)
-        api_call.assert_called_with(self.user, 'books', {"book_xml": 'publish me'})
+        api_call.assert_called_with(self.user, 'books/', {"book_xml": self.text1})
 
     @patch('apiclient.api_call')
     def test_publish_multiple(self, api_call):
         self.book[0].head.set_publishable(True)
         self.book[0].split(slug='part-2')
-        self.book[1].commit('take me \n<!-- TRIM_BEGIN -->\n too')
+        self.book[1].commit(get_fixture('chunk2.xml'))
         self.book[1].head.set_publishable(True)
         self.book.publish(self.user)
-        api_call.assert_called_with(self.user, 'books', {"book_xml": 'publish me\n too'})
+        api_call.assert_called_with(self.user, 'books/', {"book_xml": get_fixture('expected.xml')})
 
 
 class ManipulationTests(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username='tester')
-        self.book1 = Book.create(self.user, 'book 1')
-        self.book2 = Book.create(self.user, 'book 2')
+        self.book1 = Book.create(self.user, 'book 1', slug='book1')
+        self.book2 = Book.create(self.user, 'book 2', slug='book2')
 
     def test_append(self):
         self.book1.append(self.book2)
