6456ec299bf5ea9dfce8e6c278ce7e36ac08cb1f
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2012-07-05 16:18:33 +0200

    add dependency to migration

--- apps/catalogue/migrations/0010_auto__add_field_book_dc_cover_image.py
+++ apps/catalogue/migrations/0010_auto__add_field_book_dc_cover_image.py
@@ -6,6 +6,9 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (
+        ("cover", "0001_initial"),
+    )
 
     def forwards(self, orm):
         # Adding field 'Book.dc_cover_image'
