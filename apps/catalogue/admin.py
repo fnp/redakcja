f155d09d81873bcb831186335a94c5a76843a33d
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2013-07-09 10:16:41 +0200

    #2466: Assign books to projects.

--- apps/catalogue/admin.py
+++ apps/catalogue/admin.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from catalogue import models
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'public', '_published', '_new_publishable']
-    list_filter = ['public', '_published', '_new_publishable']
+    list_display = ['title', 'public', '_published', '_new_publishable', 'project']
+    list_filter = ['public', '_published', '_new_publishable', 'project']
     prepopulated_fields = {'slug': ['title']}
     search_fields = ['title']
 
 
+admin.site.register(models.Project)
 admin.site.register(models.Book, BookAdmin)
 admin.site.register(models.Chunk)
 
