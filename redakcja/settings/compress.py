ea7deea0bd4f5ca47a8e6a27f6ab640c594fdf78
Radek Czajka <radekczajka@nowoczesnapolska.org.pl>
2015-03-19 16:32:05 +0100

    Upgrade pipeline, sorl.thumbnail.

--- redakcja/settings/compress.py
+++ redakcja/settings/compress.py
@@ -2,6 +2,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'pipeline.finders.PipelineFinder',
 )
 
 
