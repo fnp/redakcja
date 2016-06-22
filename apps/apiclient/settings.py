863fcf1273511839f2e0475ec528e470d9db2ed0
Jan Szejko <jan.szejko@gmail.com>
2016-06-22 14:04:17 +0200

    api through http (not https)

--- apps/apiclient/settings.py
+++ apps/apiclient/settings.py
@@ -5,7 +5,7 @@ from django.conf import settings
 WL_CONSUMER_KEY = getattr(settings, 'APICLIENT_WL_CONSUMER_KEY', None)
 WL_CONSUMER_SECRET = getattr(settings, 'APICLIENT_WL_CONSUMER_SECRET', None)
 
-WL_API_URL = getattr(settings, 'APICLIENT_WL_API_URL', 'https://edukacjamedialna.edu.pl/api/')
+WL_API_URL = getattr(settings, 'APICLIENT_WL_API_URL', 'http://edukacjamedialna.edu.pl/api/')
 
 WL_REQUEST_TOKEN_URL = getattr(settings, 'APICLIENT_WL_REQUEST_TOKEN_URL', WL_API_URL + 'oauth/request_token/')
 WL_ACCESS_TOKEN_URL = getattr(settings, 'APICLIENT_WL_ACCESS_TOKEN_URL', WL_API_URL + 'oauth/access_token/')
