a3222a2e7997e50cf72a4548ee21c06d7e99ca65
Jan Szejko <janek37@gmail.com>
2017-09-26 16:42:20 +0200

    option to export non-publishable lesson

--- apps/catalogue/management/commands/export_lesson.py
+++ apps/catalogue/management/commands/export_lesson.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
@@ -8,7 +9,12 @@ from catalogue.models import Book
 class Command(BaseCommand):
     help = 'Exports a lesson in WL1 XML'
     args = 'slug'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '-c', '--current', action='store_false', dest='publishable', default=True,
+            help='Current version (even if not publishable)'),
+    )
 
     def handle(self, slug, *args, **options):
         lesson = Book.objects.get(slug=slug)
-        print lesson.wl1_xml()
+        print lesson.wl1_xml(publishable=options.get('publishable'))
