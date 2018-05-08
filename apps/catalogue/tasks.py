b171203911f76839a6c5da5f4123434fe4508b9f
Jan Szejko <janek37@gmail.com>
2018-05-08 17:07:36 +0200

    fix refresh task when the object is deleted

--- apps/catalogue/tasks.py
+++ apps/catalogue/tasks.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from celery.task import task
 from django.utils import translation
 
@@ -5,11 +6,15 @@ from django.utils import translation
 @task(ignore_result=True)
 def _refresh_by_pk(cls, pk, language=None):
     prev_language = translation.get_language()
-    language and translation.activate(language)
+    if language:
+        translation.activate(language)
     try:
         cls._default_manager.get(pk=pk).refresh()
+    except cls.DoesNotExist:
+        pass
     finally:
         translation.activate(prev_language)
+
 
 def refresh_instance(instance):
     _refresh_by_pk.delay(type(instance), instance.pk, translation.get_language())
