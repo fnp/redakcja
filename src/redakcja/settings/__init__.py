5489720d4099540b381b9fbc0e727c2fd8e52936
Radek Czajka <rczajka@rczajka.pl>
2021-09-07 09:32:16 +0200

    User pipeline's SASS compiler because of issue with BOM. Fixes #4239.

--- src/redakcja/settings/__init__.py
+++ src/redakcja/settings/__init__.py
@@ -124,7 +124,7 @@ PIPELINE = {
     'CSS_COMPRESSOR': None,
     'JS_COMPRESSOR': None,
     'COMPILERS': (
-        'libsasscompiler.LibSassCompiler',
+        'pipeline.compilers.sass.SASSCompiler',
     ),
 
     # CSS and JS files to compress
