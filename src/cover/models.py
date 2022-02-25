c06d9009a2f0caff4557384179078b4e45edfb8d
Radek Czajka <rczajka@rczajka.pl>
2022-02-25 14:55:42 +0100

    Cover cropping.

--- src/cover/models.py
+++ src/cover/models.py
@@ -1,6 +1,7 @@
 # This file is part of FNP-Redakcja, licensed under GNU Affero GPLv3 or later.
 # Copyright © Fundacja Nowoczesna Polska. See NOTICE for more information.
 #
+from io import BytesIO
 from django.core.files.base import ContentFile
 from django.core.files.storage import FileSystemStorage
 from django.db import models
@@ -9,6 +10,7 @@ from django.dispatch import receiver
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.sites.models import Site
+from PIL import Image as PILImage
 from cover.utils import URLOpener
 
 
@@ -27,7 +29,20 @@ class Image(models.Model):
     source_url = models.URLField(verbose_name=_('source URL'), null=True, blank=True)
     download_url = models.URLField(unique=True, verbose_name=_('image download URL'), null=True, blank=True)
     file = models.ImageField(
-        upload_to='cover/image', storage=OverwriteStorage(), editable=True, verbose_name=_('file'))
+        upload_to='cover/image',
+        storage=OverwriteStorage(),
+        verbose_name=_('file')
+    )
+    use_file = models.ImageField(
+        upload_to='cover/use',
+        storage=OverwriteStorage(),
+        editable=True,
+        verbose_name=_('file for use')
+    )
+    cut_top = models.IntegerField(default=0, )
+    cut_bottom = models.IntegerField(default=0)
+    cut_left = models.IntegerField(default=0)
+    cut_right = models.IntegerField(default=0)
 
     class Meta:
         verbose_name = _('cover image')
@@ -36,6 +51,29 @@ class Image(models.Model):
     def __str__(self):
         return u"%s - %s" % (self.author, self.title)
 
+    def save(self, **kwargs):
+        img = self.file
+        if self.cut_top or self.cut_bottom or self.cut_left or self.cut_right:
+            img = PILImage.open(img)
+            img = img.crop((
+                self.cut_left,
+                self.cut_top,
+                img.size[0] - self.cut_right,
+                img.size[1] - self.cut_bottom,
+            ))
+            buffer = BytesIO()
+            img.save(
+                buffer,
+                format='JPEG',
+            )
+            img = ContentFile(buffer.getvalue())
+        self.use_file.save(
+            "%d.jpg" % self.pk,
+            img,
+            save=False
+        )
+        super().save(**kwargs)
+    
     def get_absolute_url(self):
         return reverse('cover_image', args=[self.id])
 
