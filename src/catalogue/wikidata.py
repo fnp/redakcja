491078616e2129745f300c1f0357f5bac52e801d
Radek Czajka <rczajka@rczajka.pl>
2024-11-12 11:58:08 +0100

    Obey length limits for wikidata import.

--- src/catalogue/wikidata.py
+++ src/catalogue/wikidata.py
@@ -164,6 +164,9 @@ class WikidataModel(models.Model):
                 except:
                     pass
                 else:
+                    max_length = getattr(model_field, 'max_length', None)
+                    if max_length:
+                        wdvalue = wdvalue[:max_length]
                     setattr(self, attname, wdvalue)
 
     def wikidata_link(self):
